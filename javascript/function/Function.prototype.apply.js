1cc19d7f871244ed9d76c925e742ce930e6b486c
Ruonan Li <liruonan@hotmail.com>
2019-06-26 16:51:28 +0800

    Function.prototype.bind&call&apply

--- javascript/function/Function.prototype.apply.js
+++ javascript/function/Function.prototype.apply.js
@@ -10,3 +10,28 @@
         return min;
     }
 })(window);
+
+
+; (_ => {
+    Function.prototype.call = function (thisArg, ...args) {
+        var context = thisArg || window;
+        context.fn = this;
+
+        var result = context.fn(...args);
+        delete context.fn;
+        return result;
+    }
+    Function.prototype.apply = function (thisArg = window, argArray) {
+        thisArg.fn = this;
+        var result = thisArg.fn(...argArray);
+        delete thisArg.fn;
+        return result;
+    }
+
+    function fn(y, z) {
+        console.log(this.x + y + z);
+    }
+
+    fn.call({ x: 888 }, 100, 200);
+    fn.apply({ x: 888 }, [10000, 20000]);
+})(window);
